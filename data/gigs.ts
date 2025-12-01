088a90a54454183519676f0e26620332f3681f53
Ben Swift <ben@benswift.me>
2025-12-01 17:00:20 +1100

    add Christopher Pyne gigs data

--- data/gigs.ts
+++ data/gigs.ts
@@ -1,14 +1,122 @@
-import type { Gig } from "../.vitepress/types"
+import type { Gig } from "../.vitepress/types";
 
 export const gigs: Gig[] = [
-  // Example entry:
-  // {
-  //   role: "Board Member",
-  //   organisation: "Fortescue Metals",
-  //   category: "mining",
-  //   verified_by: "https://example.com/source",
-  //   pollie_slug: "anthony-john-abbott",
-  //   start_date: "2020-06-01",
-  //   end_date: "2022-12-31",
-  // },
-]
+  {
+    role: "Executive Chairman",
+    organisation: "Pyne and Partners",
+    category: "consulting",
+    verified_by: "AICD speaker profile",
+    source:
+      "https://www.aicd.com.au/about-aicd/authors-speakers/n-t/christopher-pyne.html",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2019-06-01",
+  },
+  {
+    role: "Independent Chair",
+    organisation: "COTA Australia",
+    category: "advocacy",
+    verified_by: "COTA Australia announcement",
+    source: "https://cota.org.au/news-items/our-new-chair/",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2024-02-15",
+  },
+  {
+    role: "Chair",
+    organisation: "Vision 2020 Australia",
+    category: "health",
+    verified_by: "mivision announcement",
+    source:
+      "https://mivision.com.au/2020/11/hon-chris-pyne-to-chair-vision-2020-australia/",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2020-11-16",
+    end_date: "2025-11-01",
+  },
+  {
+    role: "Co-Chair",
+    organisation: "Australia United Arab Emirates Business Council",
+    category: "lobbying",
+    verified_by: "AUSUAEBC website",
+    source: "https://ausuaebc.com/cochairs",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2020-11-01",
+  },
+  {
+    role: "Chair, Advisory Board",
+    organisation: "NIOA Group",
+    category: "defence",
+    verified_by: "NIOA announcement",
+    source:
+      "https://defence.nioa.com.au/latest-news/former-defence-minister-heads-up-new-nioa-board",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2020-12-01",
+  },
+  {
+    role: "Chair, SA Governors Council",
+    organisation: "American Chamber of Commerce in Australia",
+    category: "lobbying",
+    verified_by: "ICAC SA speaker bio",
+    source:
+      "https://www.icac.sa.gov.au/prevention-and-education/christopher-pyne",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2019-06-01",
+  },
+  {
+    role: "Director",
+    organisation: "International Centre for Democratic Partnerships",
+    category: "lobbying",
+    verified_by: "Michael West Media",
+    source: "https://michaelwest.com.au/christopher-pyne/",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2020-03-01",
+  },
+  {
+    role: "Non-Executive Director",
+    organisation: "Xtek Ltd",
+    category: "defence",
+    verified_by: "Proactive Investors announcement",
+    source:
+      "https://www.proactiveinvestors.com/companies/news/935169/xtek-appoints-new-non-executive-director-in-former-minister-for-defence-christopher-pyne-935169.html",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2020-11-30",
+  },
+  {
+    role: "Council Member",
+    organisation: "National Library of Australia",
+    category: "public-sector",
+    verified_by: "NLA announcement",
+    source:
+      "https://www.library.gov.au/news-media/hon-christopher-pyne-appointed-national-library-australia-council",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2025-11-20",
+  },
+  {
+    role: "Industry Professor",
+    organisation: "University of South Australia",
+    category: "education",
+    verified_by: "UniSA media release",
+    source:
+      "https://www.unisa.edu.au/Media-Centre/Releases/2019/weatherill-and-pyne-join-unisa-to-nurture--tomorrows-business-leaders/",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2019-06-07",
+  },
+  {
+    role: "Co-owner",
+    organisation: "GC Advisory",
+    category: "lobbying",
+    verified_by: "Canberra Times",
+    source:
+      "https://www.canberratimes.com.au/story/6357666/pyne-on-lobbyist-register-but-not-breaking-the-rules/",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2019-07-26",
+  },
+  {
+    role: "Advisory Board Member",
+    organisation: "Orygen Institute",
+    category: "health",
+    verified_by: "Orygen website",
+    source:
+      "https://www.orygen.org.au/Orygen-Institute/External-Advisory-Board/Christopher-Pyne",
+    pollie_slug: "christopher-maurice-pyne",
+    start_date: "2019-06-01",
+  },
+];
