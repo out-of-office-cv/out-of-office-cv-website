7e2797ef950c7ee3db3bd3c00752dd90fe4dfdb2
Ben Swift <ben@benswift.me>
2025-12-03 20:25:12 +1100

    add outofoffice.cv domain metadata

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   title: "Out of Office",
   description:
     "Tracking what Australian Parliamentarians do when they leave office",
+  head: [["link", { rel: "canonical", href: "https://outofoffice.cv" }]],
   cleanUrls: true,
   ignoreDeadLinks: false,
   themeConfig: {
